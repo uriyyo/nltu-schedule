adcb579c3732cddf730f1e20d05e200b629c96c0
Yurii Karabas <1998uriyyo@gmail.com>
2023-09-04 14:38:01 +0300

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,14 @@ import {
   Divider,
 } from "@mui/material";
 import data from "./data.json";
+import {
+  GroupsSchedules,
+  GroupSchedule,
+  ScheduleEvent,
+  SingleDayEvent,
+  HorizontalDayEvent,
+  VerticalDayEvent,
+} from "./types";
 import { useSearchParams } from "react-router-dom";
 
 const xsSizeForArray = (arr: any[]) => 12 / arr.length;
@@ -46,37 +54,37 @@ const EventBox = ({
   );
 };
 
-const EventContent = ({ children, sx, ...rest }: any) => (
-    <EventBox
-        margin={"10px"}
-        display={"flex"}
-        flexDirection={"column"}
-        justifyContent={"center"}
-        {...rest}
-    >
-        <Typography textAlign={"center"} mt={2}>
-            {children}
-        </Typography>
-    </EventBox>
+const EventContentInfo = ({ children, sx, ...rest }: any) => (
+  <EventBox
+    margin={"10px"}
+    display={"flex"}
+    flexDirection={"column"}
+    justifyContent={"center"}
+    {...rest}
+  >
+    <Typography textAlign={"center"} mt={2}>
+      {children}
+    </Typography>
+  </EventBox>
 );
 
-const VerticalEvent = ({ events }: any) => (
-    <>
-        {events.map(({type, event}: any, idx: number) => (
-            <Grid item key={idx} xs={xsSizeForArray(events)}>
-                {type === "empty" ? (
-                    <EventBox empty={true}/>
-                ) : type === "single" ? (
-                    <EventContent>{event}</EventContent>
-                ) : (
-                    <div>Unknown vertical event</div>
-                )}
-            </Grid>
-        ))}
-    </>
+const VerticalEventInfo = ({ events }: Partial<VerticalDayEvent>) => (
+  <>
+    {events?.map(({ type, event }: any, idx: number) => (
+      <Grid item key={idx} xs={xsSizeForArray(events)}>
+        {type === "empty" ? (
+          <EventBox empty={true} />
+        ) : type === "single" ? (
+          <EventContentInfo>{event}</EventContentInfo>
+        ) : (
+          <div>Unknown vertical event</div>
+        )}
+      </Grid>
+    ))}
+  </>
 );
 
-const HorizontalEvent = ({ events }: any) => {
+const HorizontalEventInfo = ({ events }: Partial<HorizontalDayEvent>) => {
   const mapHorizontalEvents = (
     { type, event, events }: any,
     nominator: boolean,
@@ -85,9 +93,9 @@ const HorizontalEvent = ({ events }: any) => {
       return <EventBox halfHeight={true} nominator={nominator} empty={true} />;
     if (type === "single")
       return (
-        <EventContent halfHeight={true} nominator={nominator}>
+        <EventContentInfo halfHeight={true} nominator={nominator}>
           {event}
-        </EventContent>
+        </EventContentInfo>
       );
 
     return (
@@ -95,7 +103,7 @@ const HorizontalEvent = ({ events }: any) => {
         {events.map((it: any, idx: number) => (
           <Grid key={idx} xs={xsSizeForArray(events)}>
             {it ? (
-              <EventContent nominator={nominator}>{it}</EventContent>
+              <EventContentInfo nominator={nominator}>{it}</EventContentInfo>
             ) : (
               <EventBox empty={true} />
             )}
@@ -107,7 +115,7 @@ const HorizontalEvent = ({ events }: any) => {
 
   return (
     <>
-      {events.map((it: any, idx: number) => (
+      {events?.map((it: any, idx: number) => (
         <Grid key={idx} item xs={12}>
           {mapHorizontalEvents(it, idx % 2 === 0)}
         </Grid>
@@ -116,68 +124,69 @@ const HorizontalEvent = ({ events }: any) => {
   );
 };
 
-const SingleEvent = ({ event }: any) => (
-    <Grid item xs={12}>
-        <EventContent>{event}</EventContent>
-    </Grid>
+const SingleEventInfo = ({ event }: Partial<SingleDayEvent>) => (
+  <Grid item xs={12}>
+    <EventContentInfo>{event}</EventContentInfo>
+  </Grid>
 );
 
-function DayEvent({ event }: any) {
-  if (event.type === "single") return <SingleEvent {...event} />;
-  if (event.type === "vertical") return <VerticalEvent {...event} />;
-  if (event.type === "horizontal") return <HorizontalEvent {...event} />;
+const DayEventInfo = ({ event }: Partial<ScheduleEvent>) => {
+  if (event?.type === "single") return <SingleEventInfo {...event} />;
+  if (event?.type === "vertical") return <VerticalEventInfo {...event} />;
+  if (event?.type === "horizontal") return <HorizontalEventInfo {...event} />;
 
   return <div>Unknown event type</div>;
-}
+};
 
-const DayInfo = ({ time, order, event }: any) => (
-    <div>
-      <h3>
-        {order} Пара (Початок {time})
-      </h3>
-      <Grid container>
-        <DayEvent event={event}></DayEvent>
-      </Grid>
-    </div>
+const DayInfo = ({ time, order, event }: ScheduleEvent) => (
+  <div>
+    <h3>
+      {order} Пара (Початок {time})
+    </h3>
+    <Grid container>
+      <DayEventInfo event={event}></DayEventInfo>
+    </Grid>
+  </div>
 );
 
-const Day = ({ day, events }: any) => (
-    <div>
-      <h2>{day}</h2>
+const Day = ({ day, events }: GroupSchedule) => (
+  <div>
+    <h2>{day}</h2>
 
-      {events.map((it: any) => (
-          <DayInfo key={it.time} {...it} />
+    {events.map((it: any) => (
+      <DayInfo key={it.time} {...it} />
+    ))}
+  </div>
+);
+
+const Schedule = ({ schedule }: { schedule: GroupSchedule[] }) => (
+  <div>
+    <h1>Розклад</h1>
+    <>
+      {schedule.map(({ day, events }: any, idx: number) => (
+        <div key={idx}>
+          <Divider />
+          <Day key={day} day={day} events={events} />
+          <br />
+        </div>
       ))}
-    </div>
+    </>
+  </div>
 );
-
-const  Schedule = ({ schedule }: any) => (
-    <div>
-      <h1>Розклад</h1>
-      <>
-        {schedule.map(({ day, events }: any, idx: number) => (
-          <div key={idx}>
-            <Divider />
-            <Day key={day} day={day} events={events} />
-            <br />
-          </div>
-        ))}
-      </>
-    </div>
-  );
-
 
 function App() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [group, setGroup] = useState(searchParams.get("group"));
-  const [schedule, setSchedule] = useState(null);
+  const [schedule, setSchedule] = useState<GroupSchedule[] | null>(null);
+
+  const allSchedules: GroupsSchedules = data as GroupsSchedules;
 
   useEffect(() => {
     if (!group) return;
 
     setSearchParams({ group });
-    setSchedule((data as any)[group]?.schedule);
-  }, [group, setSearchParams]);
+    setSchedule(allSchedules[group]?.schedule);
+  }, [group, setSearchParams, allSchedules]);
 
   return (
     <div className="App">
@@ -188,7 +197,7 @@ function App() {
           value={group}
           disablePortal
           id="group"
-          options={Object.keys(data)}
+          options={Object.keys(allSchedules)}
           onInputChange={(event, value) => setGroup(value)}
           renderInput={(params) => <TextField {...params} label="Група" />}
         />
---
+++ src/types.tsx
@@ -0,0 +1,51 @@
+export type GroupsSchedules = Record<string, Group>;
+
+export interface Group {
+  group: string;
+  subgroups: string[];
+  schedule: GroupSchedule[];
+}
+
+export interface GroupSchedule {
+  day: string;
+  events: ScheduleEvent[];
+}
+
+export interface ScheduleEvent {
+  time: string;
+  order: number;
+  event: DayEvent;
+}
+
+export interface SingleDayEvent {
+  type: "single";
+  event: string;
+}
+
+export interface VerticalDayEvent {
+  type: "vertical";
+  events: SubEvent[];
+}
+
+export interface HorizontalDayEvent {
+  type: "horizontal";
+  events: SubEvent[];
+}
+
+export type DayEvent = SingleDayEvent | VerticalDayEvent | HorizontalDayEvent;
+
+export interface SubEventEmpty {
+  type: "empty";
+}
+
+export interface SubEventSingle {
+  type: "single";
+  event: string;
+}
+
+export interface SubEventMultiple {
+  type: "multiple";
+  events: string[];
+}
+
+type SubEvent = SubEventEmpty | SubEventSingle | SubEventMultiple;
