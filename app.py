bc19b5c500e1e164170b14b568736d4a4c25af4b
Yurii Karabas <1998uriyyo@gmail.com>
2023-10-09 20:42:23 +0300

    Change schema of schedules

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-import re
 from collections import defaultdict
-from itertools import chain
-from typing import Any, TypeAlias, Sequence, Annotated, Literal, Final
+from io import BytesIO
+from typing import Any, TypeAlias, Annotated, Literal, Final
+
 import pandas as pd
 from annotated_types import Interval
-from httpx import get
-from io import BytesIO
 from fastapi import FastAPI, Query, status
 from fastapi.responses import JSONResponse
-from pydantic import AnyHttpUrl, BaseModel, AfterValidator, StringConstraints, BeforeValidator, Field
+from httpx import get
+from pydantic import AnyHttpUrl, BaseModel, AfterValidator, StringConstraints, Field
 from starlette.datastructures import URL
-
 
 EVENT_START_TIMES: Final[list[str]] = [
     "08:30",
@@ -26,27 +24,33 @@ EVENT_END_TIMES: Final[list[str]] = [
     "16:05",
     "17:35",
 ]
-SUB_EVENTS_KEYS: Final[list[str]] = [
-    "first",
-    "second",
-    "third",
-]
 SUB_EVENT_NORMALIZERS: Final[dict[str, str]] = {
     "лек.": "лекція",
     "лаб.": "лабораторна",
     "практ.": "практичні",
 }
 
-Days: TypeAlias = Annotated[
-    Literal[
-        "Понеділок",
-        "Вівторок",
-        "Середа",
-        "Четвер",
-        "П'ятниця",
-    ],
-    BeforeValidator(lambda v: "П'ятниця" if v == "Пятниця" else v),
+RAW_DAYS = [
+    "Понеділок",
+    "Вівторок",
+    "Середа",
+    "Четвер",
+    "Пятниця",
 ]
+DAYS = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+]
+RAW_DAY_TO_DAY = dict(zip(RAW_DAYS, DAYS))
+
+Days: TypeAlias = Literal[*DAYS]
+
+StartTime: TypeAlias = Literal[*EVENT_START_TIMES]
+EndTime: TypeAlias = Literal[*EVENT_END_TIMES]
+
 SubEventType: TypeAlias = Literal[
     "лекція",
     "лабораторна",
@@ -67,40 +71,18 @@ class SubEventSchema(BaseModel):
     tutor: StripedStr | None = Field(None, examples=["Процах Н.П.", "Лизанчук Т.С."])
     groups: list[StripedStr] | None = Field(None, examples=[["КН-1", "КН-2"]])
 
-    verticalFull: bool = False
-    horizontalFull: bool = False
-
-
-class SubEventsSchema(BaseModel):
-    first: SubEventSchema | None = None
-    second: SubEventSchema | None = None
-    third: SubEventSchema | None = None
-
 
 class EventSchema(BaseModel):
-    startTime: Literal[*EVENT_START_TIMES] = Field(..., examples=EVENT_START_TIMES)
-    endTime: Literal[*EVENT_END_TIMES] = Field(..., examples=EVENT_END_TIMES)
+    startTime: StartTime = Field(..., examples=EVENT_START_TIMES)
+    endTime: EndTime = Field(..., examples=EVENT_END_TIMES)
     order: Annotated[int, Interval(ge=1, le=5)] = Field(..., examples=[1, 2, 3, 4, 5])
-    nominator: SubEventsSchema | None = None
-    denominator: SubEventsSchema | None = None
+    nominator: SubEventSchema | None = None
+    denominator: SubEventSchema | None = None
+    simple: SubEventSchema | None = None
 
 
-class DayEventSchema(BaseModel):
-    day: Days
-    events: list[EventSchema]
-
-
-GroupScheduleSchema: TypeAlias = list[DayEventSchema]
-
-
-class GroupSchema(BaseModel):
-    name: StripedStr = Field(..., examples=["КН-1", "КН-2"])
-    subGroups: list[StripedStr] = Field(..., examples=[["КН-1-1", "КН-1-2"], ["КН-2-1", "КН-2-2"]])
-
-
-class RootSchedulesSchema(BaseModel):
-    groups: list[GroupSchema]
-    schedules: dict[StripedStr, GroupScheduleSchema]
+DayToEvents: TypeAlias = dict[Days, dict[StripedStr, EventSchema] | None]
+RootSchedulesSchema: TypeAlias = dict[StripedStr, DayToEvents]
 
 
 def fetch_schedule_csv_io(url: str) -> BytesIO:
@@ -126,19 +108,6 @@ def get_schedule_df(url: str) -> pd.DataFrame:
     df = df.replace("---", None)
 
     return df
-
-
-def get_groups(df: pd.DataFrame) -> dict[str, list[str]]:
-    groups = defaultdict(list)
-    for subgroup in df.columns[2:]:
-        group = re.sub(r"(\/|-)\d+.?$", "", subgroup)
-        groups[group].append(subgroup)
-
-    return groups
-
-
-def all_same(items: Sequence[Any]) -> bool:
-    return all(it == items[0] for it in items)
 
 
 def parse_subject(subject: str) -> dict[str, Any]:
@@ -182,64 +151,48 @@ def parse_event(event: str) -> dict[str, Any]:
             raise InvalidEventFormatError(f"Unexpected event format: {event!r}")
 
 
-def normalize_event(odd: list[str], even: list[str]) -> dict[str, Any] | None:
-    if all(it is None for it in chain(odd, even)):  # empty event
+def normalize_event(odd: str | None, even: str | None) -> dict[str, Any] | None:
+    if not odd and not even:  # empty event
         return None
-
-    def _parse_to_sub_events(events: list[str | None], *, type_: Literal["h", "v"], **rest: Any) -> dict[str, Any]:
-        if type_ == "h" and all_same(events):
-            rest["horizontalFull"] = True
-            events = [events[0]]
-        elif type_ == "v":
-            rest["verticalFull"] = True
-
-        return {key: rest | parse_event(val) for val, key in zip(events, SUB_EVENTS_KEYS) if val}
-
     if odd == even:
-        if all_same(odd):
-            return {
-                "nominator": {
-                    "first": {
-                        **parse_event(odd[0]),
-                        "verticalFull": True,
-                        "horizontalFull": True,
-                    }
-                }
-            }
-
-        return {
-            "nominator": _parse_to_sub_events(odd, type_="v"),
-        }
+        return {"simple": parse_event(odd)}
 
     return {
-        "nominator": _parse_to_sub_events(odd, type_="h"),
-        "denominator": _parse_to_sub_events(even, type_="h"),
+        "nominator": odd and parse_event(odd),
+        "denominator": even and parse_event(even),
     }
 
 
-def get_group_schedule(df: pd.DataFrame, subgroups: list[str]) -> Any:
-    df = df[["day", "time", *subgroups]]
-    days: dict[str, dict[str, Any]] = defaultdict(lambda: defaultdict(lambda: ([], [])))
+def prefill_missed_groups(event: dict[str, dict[str, Any]], group: str) -> dict[str, Any]:
+    for subevent in event.values():
+        if subevent and not subevent.get("groups"):
+            subevent["groups"] = [group]
 
-    for _, day, (time, week), *rest in df.itertuples():
-        for event in rest:
-            days[day][time][int(week != "ч")].append(event and event.strip())
+    return event
 
-    result = []
+
+def get_group_schedule(df: pd.DataFrame, group: str) -> Any:
+    df = df[["day", "time", group]]
+    days: dict[str, dict[str, list[Any]]] = defaultdict(lambda: defaultdict(lambda: [None, None]))
+
+    for _, day, (time, week), event in df.itertuples():
+        days[day][time][int(week != "ч")] = event and event.strip()
+
+    result = {}
     for day, times in days.items():
-        day_events = [
-            {
+        day_events = {
+            start: {
                 "startTime": start,
                 "endTime": EVENT_END_TIMES[EVENT_START_TIMES.index(start)],
                 "order": EVENT_START_TIMES.index(start) + 1,
-                **event,
+                **prefill_missed_groups(event, group),
             }
             for start, events in times.items()
             if (event := normalize_event(*events))
-        ]
+        }
 
         if day_events:
-            result.append({"day": day, "events": day_events})
+            result[RAW_DAY_TO_DAY[day]] = day_events
 
     return result
 
@@ -268,7 +221,7 @@ SheetUrl: TypeAlias = Annotated[
 app = FastAPI(
     title="NLTU Schedule Parsing API",
     description="API for parsing NLTU schedule from Google Sheets",
-    version="0.1.0",
+    version="0.2.0",
 )
 
 app.add_exception_handler(
@@ -280,7 +233,10 @@ app.add_exception_handler(
 )
 
 
-@app.get("/schedules")
+@app.get(
+    "/schedules",
+    response_model=RootSchedulesSchema,
+)
 def get_schedule(
     *,
     sheet_url: SheetUrl = Query(
@@ -288,16 +244,11 @@ def get_schedule(
         description="URL of the schedule sheet",
         annotation=SheetUrl,
     ),
-) -> RootSchedulesSchema:
+) -> Any:
     df = get_schedule_df(str(sheet_url))
-    groups = get_groups(df)
+    groups = [*df.columns[2:]]
 
-    return RootSchedulesSchema.model_validate(
-        {
-            "groups": [{"name": group, "subGroups": subgroups} for group, subgroups in groups.items()],
-            "schedules": {group: get_group_schedule(df, subgroups) for group, subgroups in groups.items()},
-        }
-    )
+    return {group: get_group_schedule(df, group) for group in groups}
 
 
 if __name__ == "__main__":
